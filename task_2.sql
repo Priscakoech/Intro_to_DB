56c06a50d1d1786625783790517c03cbf7487fcb
Priscakoech <priscakoech448@gmail.com>
2024-10-27 20:19:23 +0300

    Add script

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,5 @@
 -- Creating the books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Creating the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Creating the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Creating the order_details table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
