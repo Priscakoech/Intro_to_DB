bb05b8e4040d02ba6b9c30cc68b4a4e14b9ac338
Priscakoech <priscakoech448@gmail.com>
2024-10-27 19:50:37 +0300

    Add script for database

---
+++ MySQLServer.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database(database_name):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password"
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor._execute(f"CREATE DATABASE IF NOT EXISTS {database_name};")
+            print(f"Database '{database_name}' created successfully!")
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+            
+if __name__ == "__main__":
+    create_database("alx_book_store")
