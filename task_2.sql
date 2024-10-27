7f3a79c71639ff600d84bc9f36ad7c978743da0e
Priscakoech <priscakoech448@gmail.com>
2024-10-27 20:28:01 +0300

    Add script

--- task_2.sql
+++ task_2.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 -- Creating the order_details table
-CREATE TABLE IF NOT EXISTS Order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
